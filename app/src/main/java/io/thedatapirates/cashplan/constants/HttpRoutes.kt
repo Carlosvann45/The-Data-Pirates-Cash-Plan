2a3bb6e2b1b122d42d28cc584af0965c5aac7180
carlos <clsantiago@student.fullsail.edu>
2022-08-11 23:08:51 -0400

    fixed url link for backend

--- app/src/main/java/io/thedatapirates/cashplan/constants/HttpRoutes.kt
+++ app/src/main/java/io/thedatapirates/cashplan/constants/HttpRoutes.kt
@@ -4,7 +4,7 @@ package io.thedatapirates.cashplan.constants
  * A class for constant string variables
  */
 object HttpRoutes {
-    private const val BASE_URL = "http://192.168.1.35:8085"
+    private const val BASE_URL = "https://the-data-pirates-cash-plan.herokuapp.com"
     const val LOGIN = "$BASE_URL/customers/login"
     const val CUSTOMERS = "$BASE_URL/customers"
     const val FORGOT_PASSWORD = "$BASE_URL/verifications/password/forgot"
