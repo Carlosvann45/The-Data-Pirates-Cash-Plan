ddfe12681d84a64820e8e12da4020794d3b36c57
Jeremy Dickerson <jtpilot20@gmail.com>
2022-08-17 22:55:16 -0500

    initail commit for notification page

---
+++ app/src/main/java/io/thedatapirates/cashplan/domains/settings/StNotifications.kt
@@ -0,0 +1,26 @@
+package io.thedatapirates.cashplan.domains.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.thedatapirates.cashplan.R
+
+/**
+ * A simple [Fragment] subclass.
+ * create an instance of this fragment.
+ */
+class StNotifications : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_st_notifications, container, false)
+    }
+}
