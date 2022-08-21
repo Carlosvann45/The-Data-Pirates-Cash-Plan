989d73d47fccacfbe82f16bcc989f484f0b78054
Jeremy Dickerson <jtpilot20@gmail.com>
2022-08-20 23:03:09 -0500

    finished layout pt 2

--- app/src/main/java/io/thedatapirates/cashplan/domains/settings/StNotifications.kt
+++ app/src/main/java/io/thedatapirates/cashplan/domains/settings/StNotifications.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import io.thedatapirates.cashplan.R
+import kotlinx.android.synthetic.main.fragment_settings.view.*
+import kotlinx.android.synthetic.main.fragment_st_notifications.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -21,6 +24,11 @@ class StNotifications : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_st_notifications, container, false)
+        val view = inflater.inflate(R.layout.fragment_st_notifications, container, false)
+        view.stNotifTv_AlertCustom.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.stNotif_to_stMenu)
+        }
+
+        return view
     }
 }
