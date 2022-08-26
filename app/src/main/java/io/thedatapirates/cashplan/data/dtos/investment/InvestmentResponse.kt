700af861d11045c73c216e42317b7092ac7b4e47
carlos <clsantiago@student.fullsail.edu>
2022-08-25 22:35:16 -0400

    removed parcebal implmentation from investment response

--- app/src/main/java/io/thedatapirates/cashplan/data/dtos/investment/InvestmentResponse.kt
+++ app/src/main/java/io/thedatapirates/cashplan/data/dtos/investment/InvestmentResponse.kt
@@ -17,34 +17,5 @@ class InvestmentResponse(
     var sector: String = "",
     var amount: Double = 0.00,
     var buyPrice: Double = 0.00
-): Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.readString().toString(),
-        parcel.readString().toString(),
-        parcel.readString().toString(),
-        parcel.readString().toString(),
-        parcel.readString().toString(),
-        parcel.readDouble(),
-        parcel.readDouble()
-    ) {
-    }
-    companion object CREATOR : Parcelable.Creator<InvestmentResponse> {
-        override fun createFromParcel(parcel: Parcel): InvestmentResponse {
-            return InvestmentResponse(parcel)
-        }
-
-        override fun newArray(size: Int): Array<InvestmentResponse?> {
-            return arrayOfNulls(size)
-        }
-    }
-
-    override fun describeContents(): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun writeToParcel(p0: Parcel?, p1: Int) {
-        TODO("Not yet implemented")
-    }
-
+) {
 }
