2b4ebd272c7f977a1a477953435ed04000115491
aric-hylton <97090893+aric-hylton@users.noreply.github.com>
2022-08-28 17:08:56 -0500

    Cash flow (#9)
    * Added CashFlow to the Navigation pages with a button on the home page to get to the Cashflow Screen as well as getting back to the Home Page
    
    * Added CashFlow to the Navigation pages with a button on the home page to get to the Cashflow Screen as well as getting back to the Home Page. Add layout prototype design elements to CashFlow Page
    
    * Added CashFlow to the Navigation pages with a button on the home page to get to the Cashflow Screen as well as getting back to the Home Page. Add layout prototype design elements to CashFlow Page. Updated the strings for the fragment_cashflow.
    
    * Added CashFlow to the Navigation pages with a button on the home page to get to the Cashflow Screen as well as getting back to the Home Page. Add layout prototype design elements to CashFlow Page. Updated the strings for the fragment_cashflow. Adding Functionality to the payments
    
    * Added CashFlow to the Navigation pages with a button on the home page to get to the Cashflow Screen as well as getting back to the Home Page. Add layout prototype design elements to CashFlow Page. Updated the strings for the fragment_cashflow. Adding Functionality to the payments
    
    * Added CashFlow to the Navigation pages with a button on the home page to get to the Cashflow Screen as well as getting back to the Home Page. Add layout prototype design elements to CashFlow Page. Updated the strings for the fragment_cashflow. Adding Functionality to the payments and deposits
    
    * Added CashFlow to the Navigation pages with a button on the home page to get to the Cashflow Screen as well as getting back to the Home Page. Add layout prototype design elements to CashFlow Page. Updated the strings for the fragment_cashflow. Adding Functionality to the payments and deposits
    
    Created 2 seperate fragments to handle creating a deposit/expense and viewing the users deposits and expenses
    
    * Added CashFlow to the Navigation pages with a button on the home page to get to the Cashflow Screen as well as getting back to the Home Page. Add layout prototype design elements to CashFlow Page. Updated the strings for the fragment_cashflow. Adding Functionality to the payments and deposits
    
    Created 2 seperate fragments to handle creating a deposit/expense and viewing the users deposits and expenses
    
    * Added CashFlow to the Navigation pages with a button on the home page to get to the Cashflow Screen as well as getting back to the Home Page. Add layout prototype design elements to CashFlow Page. Updated the strings for the fragment_cashflow. Adding Functionality to the payments and deposits
    
    Created 2 seperate fragments to handle creating a deposit/expense and viewing the users deposits and expenses
    Fixed conflicts with strings.xml

--- app/src/main/java/io/thedatapirates/cashplan/domains/cashflow/CashFlowFragment.kt
+++ app/src/main/java/io/thedatapirates/cashplan/domains/cashflow/CashFlowFragment.kt
@@ -1,33 +1,66 @@
 package io.thedatapirates.cashplan.domains.cashflow
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import io.thedatapirates.cashplan.R
-import kotlinx.android.synthetic.main.fragment_cash_flow.view.*
+import kotlinx.android.synthetic.main.fragment_cash_flow.*
+import kotlinx.android.synthetic.main.fragment_nested_cash_flow.*
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CashFlowFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class CashFlowFragment : Fragment() {
-
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>? = null
+    private lateinit var cashFlowAdapter: CashFlowAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_cash_flow, container, false)
-
-        view.tvCashFlowText.setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.navNestedCashFlow)
-        }
-
-        return view
+        return inflater.inflate(R.layout.fragment_cash_flow, container, false)
     }
 
+
+    override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
+        cashFlowAdapter = CashFlowAdapter(mutableListOf())
+
+        setFragmentResultListener("requestKey") { key, bundle ->
+            // Any type can be passed via to the bundle
+            val itemName = bundle.getString("name")
+            val itemOption = bundle.getString("option")
+            val itemAmount = bundle.getFloat("amount")
+
+            if (itemOption != null  && itemName != null) {
+                val item = CashFlowItem(itemOption, itemName, itemAmount)
+                cashFlowAdapter.addExpense(item)
+            }
+        }
+
+        super.onViewCreated(itemView, savedInstanceState)
+        rvExpenses.apply {
+            layoutManager = LinearLayoutManager(this.context)
+            adapter = cashFlowAdapter
+
+        }
+        tvExpensesTotal.text = cashFlowAdapter.expensesTotal().toString()
+
+        btnAddExpense.setOnClickListener {
+            Navigation.findNavController(itemView).navigate(R.id.rlNestedCashFlowFragment)
+        }
+
+        btnDeleteDoneExpenses.setOnClickListener {
+            cashFlowAdapter.deleteItems()
+            tvExpensesTotal.text = cashFlowAdapter.expensesTotal().toString()
+        }
+    }
 }
