3794ec26ef19f70d44c0d238b7206dc19b4e5810
Aric Hylton <Ahylton@student.fullsail.edu>
2022-08-28 16:47:16 -0500

    Added CashFlow to the Navigation pages with a button on the home page to get to the Cashflow Screen as well as getting back to the Home Page. Add layout prototype design elements to CashFlow Page. Updated the strings for the fragment_cashflow. Adding Functionality to the payments and deposits
    Created 2 seperate fragments to handle creating a deposit/expense and viewing the users deposits and expenses

---
+++ app/src/main/java/io/thedatapirates/cashplan/domains/cashflow/CashFlowItem.kt
@@ -0,0 +1,11 @@
+package io.thedatapirates.cashplan.domains.cashflow
+
+data class CashFlowItem (
+    var type: String = "Deposit",
+    var name: String = "",
+    var amount: Float,
+    var isMonthly: Boolean = false,
+    var isChecked: Boolean = false
+
+
+)
